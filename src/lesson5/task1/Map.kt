7180c332cd8c0cd7ef576557663ae34a124edc09
mariaromanikhina <rom.maria04@mail.ru>
2022-11-04 01:27:07 +0300

    Map added

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -126,7 +126,6 @@ fun containsIn(a: Map<String, String>, b: Map<String, String>): Boolean {
         }
     }
     return ret == 0
-    return false
 }
 
 
@@ -163,6 +162,9 @@ fun whoAreInBoth(a: List<String>, b: List<String>): List<String> {
     var new = mutableListOf<String>()
     for (it in a) {
         for (them in b) {
+            if (new.contains(it)) {
+                break
+            }
             if (it == them) {
                 new.add(it)
                 break
@@ -210,7 +212,26 @@ fun mergePhoneBooks(mapA: Map<String, String>, mapB: Map<String, String>): Map<S
  *   averageStockPrice(listOf("MSFT" to 100.0, "MSFT" to 200.0, "NFLX" to 40.0))
  *     -> mapOf("MSFT" to 150.0, "NFLX" to 40.0)
  */
-fun averageStockPrice(stockPrices: List<Pair<String, Double>>): Map<String, Double> = TODO()
+fun averageStockPrice(stockPrices: List<Pair<String, Double>>): Map<String, Double> {
+    var new = mutableMapOf<String, Double>()
+    for (item in stockPrices) {
+        var sum = 0.0
+        var count = 0
+        var string = item.first
+        if (new.containsKey(string)) {
+            continue
+        }
+        for (i in stockPrices) {
+            if (i.first == string) {
+                sum += i.second
+                count++
+            }
+        }
+        sum /= count
+        new.put(string, sum)
+    }
+    return new
+}
 
 /**
  * Средняя (4 балла)
@@ -227,7 +248,26 @@ fun averageStockPrice(stockPrices: List<Pair<String, Double>>): Map<String, Doub
  *     "печенье"
  *   ) -> "Мария"
  */
-fun findCheapestStuff(stuff: Map<String, Pair<String, Double>>, kind: String): String? = TODO()
+fun findCheapestStuff(stuff: Map<String, Pair<String, Double>>, kind: String): String? {
+    var minName = ""
+    var minCost = 0.0
+    var flag = false
+    stuff.forEach { (s, i) ->
+        if (kind == i.first) {
+            if (!flag) {
+                minName = s
+                minCost = i.second
+                flag = true
+            }
+            else if (i.second < minCost) {
+                minName = s
+                minCost = i.second
+            }
+        }
+    }
+    if (!flag) return null
+    else return minName
+}
 
 /**
  * Средняя (3 балла)
